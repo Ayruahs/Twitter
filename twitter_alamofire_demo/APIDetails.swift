1b1bc0d93d642298e9059879e7e74a909bf3045c
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2018-02-10 14:54:38 -0500

    added apidetails file

---
+++ twitter_alamofire_demo/APIDetails.swift
@@ -0,0 +1,14 @@
+//
+//  APIDetails.swift
+//  twitter_alamofire_demo
+//
+//  Created by Shaurya Sinha on 10/02/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import Foundation
+
+class APIDetails{
+    static let consumerKey = "GWFjYKsfQ8vM7SsFVr8y8fkMH"
+    static let consumerSecret = "7F7yAC8MKghq1ncWEoxbkekO4yNRqugJgAOSIF9PUuEItU3CRb"
+}
